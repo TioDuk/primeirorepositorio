bd659e7ec9fdd9340c46260b1d82a3de8b9e8ac3
TioDuk <99151575+TioDuk@users.noreply.github.com>
2022-02-09 20:56:23 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipoNumber
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoflutuante = 7.4;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoflutuante;
+
+console.log(novaOperacao)
+
+// NaN Not a Number
+
+const alura = 'alura';
+
+console.log(alura * primeiroNumero)
