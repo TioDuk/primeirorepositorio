cdbfd14d0ca2c3580401c859c405bdce6af4bc56
TioDuk <danilo_12346@hotmail.com>
2022-02-17 20:00:25 -0300

    codigos var e conversoes

---
+++ curso-2/conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversao implicíta
+const numero = 456;
+const numeroString = "456";
+
+// Number
+// String
+
+// conversao explicíta
+
+console.log(numero + Number(numeroString));
