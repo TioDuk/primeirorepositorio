3a851d67cb209eb8f8fd9a9db6d8dfbcaedc74b0
TioDuk <danilo_12346@hotmail.com>
2022-02-24 19:24:48 -0300

    Exercicios Alura

---
+++ console.js
@@ -0,0 +1,11 @@
+// log -> registro
+
+const minhaVar = true;
+
+//console.log(12346)
+//console.log("Deus não existe")
+//console.log(minhaVar)
+
+// tratamento de erros!
+
+console.error("tem alguma coisa errada ai!")
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+//if(idadeCliente >= idadeMinina) {
+  //  console.log('Cerveja Artesanal')
+//} else {
+  //  console.log('Leite')
+//}
+
+            // condiçao                   //true    //false
+console.log(idadeCliente >= idadeMinima ? "Breja" : "Suco")
